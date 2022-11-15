e5bb5aa4595e02e1a587d409f597ffc56b7d1d02
Phil Wilkinson <phil@philwilky.me>
2022-11-15 11:49:06 +0000

    initial code

---
+++ script.js
@@ -0,0 +1,66 @@
+let searchBtn = document.getElementById("search-btn"); // search button
+let countryInp = document.getElementById("country-inp") // input field
+
+// listens for search button click
+searchBtn.addEventListener("click",() => {
+    CallApi();
+});
+
+//listens for enter key pressed
+countryInp.addEventListener("keydown",({key}) => {
+    if (key === "Enter")
+    {
+        CallApi();
+    } else return
+});
+
+
+//main function that calls the api
+function CallApi(){
+    let countryName = countryInp.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    fetch(finalURL).then((response) => response.json()).then((data) => {
+        var popCleaned = data[0].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        result.innerHTML = `
+        <img src="${data[0].flags.svg}"
+        class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${popCleaned}</span>
+            </div
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
+            </div
+        </div>
+        <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Common Languages:</h4>
+            <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+        </div
+    </div>
+        `
+    }).catch(()=> {
+        if (countryName.length == 0) {
+            result.innerHTML = `<h3> The input field cannot be empty </h3>`
+        } else {
+            result.innerHTML = `<h3> Please enter a vaild country name.</h3>`
+        }
+    })
+}
